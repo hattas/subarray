6da9c3cb60de0143ab05883710db5cca72e5df3c
John Hattas <johnhattas@gmail.com>
2019-03-28 22:35:13 -0500

    update find_maximum_subarray to have option to return dict instead of tuple

--- algs.py
+++ algs.py
@@ -18,7 +18,7 @@ def _find_max_crossing_subarray(A, low, mid, high):
             max_right = j
     return (max_left, max_right, left_sum + right_sum)
 
-def find_maximum_subarray(A, low=None, high=None):
+def find_maximum_subarray(A, low=None, high=None, return_type=dict):
     '''p72 Introduction To Algorithms'''
     if low == None:
         low = 1 # low is 1 because A[0] is NaN
@@ -26,15 +26,22 @@ def find_maximum_subarray(A, low=None, high=None):
         high = len(A) - 1
     
     if high == low:
-        return (low, high, A[low])
+        return_low, return_high, return_sum = low, high, A[low]
+    elif high < low:
+        return_low, return_high, return_sum = low, high, 0
     else:
         mid = int(np.floor((low + high) / 2))
-        left_low, left_high, left_sum = find_maximum_subarray(A, low, mid)
-        right_low, right_high, right_sum = find_maximum_subarray(A, mid + 1, high)
+        left_low, left_high, left_sum = find_maximum_subarray(A, low, mid, tuple)
+        right_low, right_high, right_sum = find_maximum_subarray(A, mid + 1, high, tuple)
         cross_low, cross_high, cross_sum = _find_max_crossing_subarray(A, low, mid, high)
         if left_sum >= right_sum and left_sum >= cross_sum:
-            return (left_low, left_high, left_sum)
+            return_low, return_high, return_sum = left_low, left_high, left_sum
         elif right_sum >= left_sum and right_sum >= cross_sum:
-            return (right_low, right_high, right_sum)
+            return_low, return_high, return_sum = right_low, right_high, right_sum
         else:
-            return (cross_low, cross_high, cross_sum)
+            return_low, return_high, return_sum = cross_low, cross_high, cross_sum
+            
+    if return_type == tuple:
+        return (return_low, return_high, return_sum)
+    else:
+        return {'low':return_low, 'high':return_high, 'sum':return_sum}
